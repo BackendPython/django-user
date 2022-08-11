c065ee86f8784dde1fe4418d9dbcf49dc075552c
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-11 19:07:16 +0500

    main

--- website/models.py
+++ website/models.py
@@ -3,3 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class Admin(AbstractUser):
     logo = models.ImageField()
+    file = models.FileField()
+    file_name = models.CharField(max_length=100)
