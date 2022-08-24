dd45444e9fcb7168833244be429d6fe7771d955e
backendpython <holmirzaevsohruh196@gmail.com>
2022-08-24 15:31:23 +0500

    main

--- down/settings.py
+++ down/settings.py
@@ -118,6 +118,8 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
